dd9ed7c4820d9e89e17159f65ab311fb84242066
nikemaks <javaker007@gmail.com>
2024-02-29 23:46:36 +0100

    Add page full notes

---
+++ src/app/components/full-notes/notes-view/notes-view.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotesViewComponent } from './notes-view.component';
+
+describe('NotesViewComponent', () => {
+  let component: NotesViewComponent;
+  let fixture: ComponentFixture<NotesViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NotesViewComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NotesViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
