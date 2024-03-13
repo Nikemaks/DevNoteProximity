308f34710e8fe80b1fc26183be7a0f8a93162c51
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-03-13 19:06:02 +0200

    feat: add ui for view note

--- src/app/components/full-notes/notes-view/notes-view.component.ts
+++ src/app/components/full-notes/notes-view/notes-view.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FullNotesStoreService } from '../../../store/full-notes-store/full-notes-store.service';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-notes-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './notes-view.component.html',
   styleUrl: './notes-view.component.scss',
 })
