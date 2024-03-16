b6edbc15763ec83b592ee58fadafa3e6777adb32
Roman15122022 <120688434+Roman15122022@users.noreply.github.com>
2024-03-16 02:42:02 +0200

    feat: new page

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -3,12 +3,35 @@ import { TranslateModule } from '@ngx-translate/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { RouterLink } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [TranslateModule, MatIconModule, MatCheckboxModule, RouterLink],
+  imports: [
+    TranslateModule,
+    MatIconModule,
+    MatCheckboxModule,
+    RouterLink,
+    ReactiveFormsModule,
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.scss',
 })
-export class SignInComponent {}
+export class SignInComponent {
+  loginForm: FormGroup = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+  }
+}
