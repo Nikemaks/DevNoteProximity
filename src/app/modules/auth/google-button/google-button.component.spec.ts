af143b2d532663b2674a094264bc7f7adf7599ac
nikemaks <javaker007@gmail.com>
2024-04-10 23:57:07 +0200

    Add registration and fix auth. #71

---
+++ src/app/modules/auth/google-button/google-button.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GoogleButtonComponent } from './google-button.component';
+
+describe('GoogleButtonComponent', () => {
+  let component: GoogleButtonComponent;
+  let fixture: ComponentFixture<GoogleButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GoogleButtonComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(GoogleButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -3,12 +3,58 @@ import { TranslateModule } from '@ngx-translate/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { RouterLink } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../../services/auth/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { GoogleButtonComponent } from '../google-button/google-button.component';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [TranslateModule, MatIconModule, MatCheckboxModule, RouterLink],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    RouterLink,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    GoogleButtonComponent,
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  formGroup: FormGroup = this._fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(
+    private _fb: FormBuilder,
+    private authService: AuthService
+  ) {}
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    if (this.formGroup.invalid) return;
+    const { email, password } = this.formGroup.value;
+
+    this.authService.signUpWithEmail(email, password);
+  }
+
+  googleLogIn(event: Event) {
+    event.preventDefault();
+    this.authService.googleModalLogIn();
+  }
+}
